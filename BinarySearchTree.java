d48d9b0d0d586a8423d15a3f8dd9267749409766
Steve Aronson <saronson@lfschools.net>
2021-01-27 19:59:14 -0600

    Update and rename BinaryTree.java to BinarySearchTree.java

---
+++ BinarySearchTree.java
@@ -0,0 +1,183 @@
+// BinarySearchTree class
+//
+// CONSTRUCTION: with no initializer
+//
+// ******************PUBLIC OPERATIONS*********************
+// void insert( x )       --> Insert x
+// void remove( x )       --> Remove x
+// void removeMin( )      --> Remove minimum item
+// Comparable find( x )   --> Return item that matches x
+// Comparable findMin( )  --> Return smallest item
+// Comparable findMax( )  --> Return largest item
+// boolean isEmpty( )     --> Return true if empty; else false
+// void makeEmpty( )      --> Remove all items
+// ******************ERRORS********************************
+// Exceptions are thrown by insert, remove, and removeMin if warranted
+
+/**
+ * Implements an unbalanced binary search tree.
+ * Note that all "matching" is based on the compareTo method.
+ * @author Mark Allen Weiss/Aronson
+ */
+public class BinarySearchTree {
+    /** The tree root. */
+    protected BinaryNode root;
+
+    /**
+     * Construct the tree.
+     */
+    public BinarySearchTree( ) {
+        root = null;
+    }
+
+    /**
+     * Make the tree logically empty.
+     */
+    public void makeEmpty( ) {
+        root = null;
+    }
+
+    /**
+     * Test if the tree is logically empty.
+     * @return true if empty, false otherwise.
+     */
+    public boolean isEmpty( ) {
+        return root == null;
+    }
+
+    /**
+     * Internal method to get element field.
+     * @param t the node.
+     * @return the element field or null if t is null.
+     */
+    private Comparable elementAt( BinaryNode t ) {
+        return t == null ? null : t.element;
+    }
+
+    /**
+     * Insert into the tree.
+     * @param x the item to insert.
+     * @throws DuplicateItemException if x is already present.
+     */
+    public void insert( Comparable x ) {
+        root = insert( x, root );
+    }
+
+    /**
+     * Internal method to insert into a subtree.
+     * @param x the item to insert.
+     * @param t the node that roots the tree.
+     * @return the new root.
+     * @throws DuplicateItemException if x is already present.
+     */
+    protected BinaryNode insert( Comparable x, BinaryNode t ) {
+        if (true)
+            ; // find spot otherwise
+        else // duplicate
+            throw new DuplicateItemException( x.toString( ) );
+        return t;
+    }
+
+    /**
+     * Find the smallest item in the tree.
+     * @return smallest item or null if empty.
+     */
+    public Comparable findMin( ) {
+        return elementAt( findMin( root ) );
+    }
+
+    /**
+     * Internal method to find the smallest item in a subtree.
+     * @param t the node that roots the tree.
+     * @return node containing the smallest item.
+     */
+    protected BinaryNode findMin( BinaryNode t ) {
+
+        return t;
+    }
+
+    /**
+     * Find the largest item in the tree.
+     * @return the largest item or null if empty.
+     */
+    public Comparable findMax( ) {
+        return elementAt( findMax( root ) );
+    }
+
+    /**
+     * Internal method to find the largest item in a subtree.
+     * @param t the node that roots the tree.
+     * @return node containing the largest item.
+     */
+    private BinaryNode findMax( BinaryNode t ) {
+
+        return t;
+    }
+
+    /**
+     * Find an item in the tree.
+     * @param x the item to search for.
+     * @return the matching item or null if not found.
+     */
+    public Comparable find( Comparable x ) {
+        return elementAt( find( x, root ) );
+    }
+
+    /**
+     * Internal method to find an item in a subtree.
+     * @param x is item to search for.
+     * @param t the node that roots the tree.
+     * @return node containing the matched item.
+     */
+    private BinaryNode find( Comparable x, BinaryNode t ) {
+
+        return null;         // Not found
+    }
+
+    /**
+     * Remove from the tree..
+     * @param x the item to remove.
+     * @throws ItemNotFoundException if x is not found.
+     */
+    public void remove( Comparable x ) {
+        root = remove( x, root );
+    }
+
+    /**
+     * Internal method to remove from a subtree.
+     * @param x the item to remove.
+     * @param t the node that roots the tree.
+     * @return the new root.
+     * @throws ItemNotFoundException if x is not found.
+     */
+    protected BinaryNode remove( Comparable x, BinaryNode t ) {
+        if( t == null )
+            throw new ItemNotFoundException( x.toString( ) );
+
+        return t;
+    }
+
+    /**
+     * Remove minimum item from the tree.
+     * @throws ItemNotFoundException if tree is empty.
+     */
+    public void removeMin( ) {
+        root = removeMin( root );
+    }
+
+    /**
+     * Internal method to remove minimum item from a subtree.
+     * @param t the node that roots the tree.
+     * @return the new root.
+     * @throws ItemNotFoundException if x is not found.
+     */
+    protected BinaryNode removeMin( BinaryNode t ) {
+        if( t == null )
+            throw new ItemNotFoundException( );
+
+        return t;
+    }
+
+}
+
+
